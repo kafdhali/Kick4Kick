62de8c7b98ab2a1518357df179cbf47ed0837f5d
kafdhali <khaledafdh075@gmail.com>
2024-04-24 12:59:18 +0200

    2.1

--- script.js
+++ script.js
@@ -7,31 +7,38 @@ let botScore = document.getElementById("botScore");
 
 let ArrowUpPressed = false;
 let ArrowDownPressed = false;
+let ArrowLeftPressed = false;
+let ArrowRightPressed = false;
+
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
 
+
 function keyDownHandler(e) {
-    if (e.key == 'ArrowUp') {
+    if (e.key === 'ArrowUp') {
         ArrowUpPressed = true;
-        // console.log("z pressed");
-    }
-    else if (e.key == 'ArrowDown') {
+    } else if (e.key === 'ArrowDown') {
         ArrowDownPressed = true;
-        // console.log("x pressed");
+    } else if (e.key === 'ArrowLeft') {
+        ArrowLeftPressed = true;
+    } else if (e.key === 'ArrowRight') {
+        ArrowRightPressed = true;
     }
 }
 
 function keyUpHandler(e) {
-    if (e.key == 'ArrowUp') {
+    if (e.key === 'ArrowUp') {
         ArrowUpPressed = false;
-        // console.log("z released");
-    }
-    else if (e.key == 'ArrowDown') {
+    } else if (e.key === 'ArrowDown') {
         ArrowDownPressed = false;
-        // console.log("x released");
+    } else if (e.key === 'ArrowLeft') {
+        ArrowLeftPressed = false;
+    } else if (e.key === 'ArrowRight') {
+        ArrowRightPressed = false;
     }
 }
+
 
 
     // initialize the velocity of the ball
@@ -44,8 +51,8 @@ function reset () {
     
     ball.style.left = "50%";
     ball.style.top = "50%";
-    vx = 3;
-    vy = 4;
+    vx = -5;
+    vy = -6;
     v = Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
 }
 
@@ -61,6 +68,7 @@ function detectCollision (currentPaddle){
     let paddlebottom = currentPaddle.offsetTop + currentPaddle.offsetHeight;    
     let paddleleft = currentPaddle.offsetLeft;    
     let paddleright = currentPaddle.offsetLeft + currentPaddle.offsetWidth;    
+    
     //
     if( ballbottom > paddletop && 
         balltop < paddlebottom &&
@@ -79,6 +87,7 @@ function detectCollision (currentPaddle){
 function mechanic(){
     if(ball.offsetLeft <0) {
         botScore.innerHTML = parseInt(botScore.innerHTML) +1; //this will update the botscore whenever the player 1 lose
+        whistleSound.play();
         reset();
         
         vx= -vx ;
@@ -86,6 +95,7 @@ function mechanic(){
     }
     if(ball.offsetLeft > container.offsetWidth - ball.offsetWidth) {
         userscore.innerHTML = parseInt(userscore.innerHTML) + 1; ////this will update the player 1 whenever the bot lose
+        whistleSound.play();
         reset();
         vx = -vx;
     }
@@ -95,18 +105,20 @@ function mechanic(){
     if(ball.offsetTop > container.offsetHeight - ball.offsetHeight) {
         vy = -vy;
     }
-    
+    // make the paddle dont across the hight of the container
     let paddle = ball.offsetLeft < container.offsetWidth / 2 ? player1 : botpaddle;
-    let ballcenterY = ball.offsetTop + ball.offsetHeight / 2;
-    let paddlecenterY = paddle.offsetTop + paddle.offsetHeight / 2;
-    let angle = 0;
+    
+    let ballcenterY = ball.offsetTop + ball.offsetHeight / 2; // getting the value between the center of ball and the top of the container
+    let paddlecenterY = paddle.offsetTop + paddle.offsetHeight / 2; //getting the value between the center of paddle and the top of the container
+    let angle = 0; // we will use it to create the ball reflaction angle
        
 
 
     if (detectCollision(paddle)) {
-        if (paddle === player1) {
+        // if the ball is in the player1 side
+        if (paddle === player1) { 
             if (ballcenterY < paddlecenterY) {
-                angle = -Math.PI / 4;
+                angle = -Math.PI / 4; 
             }
             else if (ballcenterY > paddlecenterY) {
                 angle = Math.PI / 4;
@@ -114,7 +126,9 @@ function mechanic(){
             else {
                 angle = 0;
             }
+            hitSound.play();
         }
+        // if the ball is in the botpaddle side
         else if (paddle === botpaddle) {
             if (ballcenterY < paddlecenterY) {
                 angle = -3 * Math.PI / 4;
@@ -125,33 +139,35 @@ function mechanic(){
             else {
                 angle = 0;
             }
+            hitSound.play();
         }
-        v = v + 0.5;
+        v = v + 0.6;
         vx = v * Math.cos(angle);
         vy = v * Math.sin(angle);
     }
 
-    let aidelay = 0.3;
+    let errorDelay = 0.5;
     botpaddle.style.top =
-    botpaddle.offsetTop + (ball.offsetTop - botpaddle.offsetTop - botpaddle.offsetHeight / 2) * aidelay + "px";
+    botpaddle.offsetTop + (ball.offsetTop - botpaddle.offsetTop - botpaddle.offsetHeight / 2) * errorDelay + "px";
 
 
     ball.style.left = ball.offsetLeft + vx + "px";
     ball.style.top = ball.offsetTop + vy + "px";
-    if (ArrowUpPressed && player1.offsetTop > 55) {
-        player1.style.top = player1.offsetTop - 5 + "px";
+    const paddleSpeed = 5; // Adjust speed as needed
+    if (ArrowUpPressed && player1.offsetTop > 0) {
+        player1.style.top = player1.offsetTop - paddleSpeed + 'px';
     }
-    if (ArrowDownPressed && player1.offsetTop < container.offsetHeight
-        - player1.offsetHeight + 45) {
-        player1.style.top = player1.offsetTop + 5 + "px";
+    if (ArrowDownPressed && player1.offsetTop + player1.offsetHeight < container.offsetHeight) {
+        player1.style.top = player1.offsetTop + paddleSpeed + 'px';
     }
-    
-    requestAnimationFrame(mechanic)
+    if (ArrowLeftPressed && player1.offsetLeft > 0) {
+        player1.style.left = player1.offsetLeft - paddleSpeed + 'px';
+    }
+    if (ArrowRightPressed && player1.offsetLeft + player1.offsetWidth < container.offsetWidth) {
+        player1.style.left = player1.offsetLeft + paddleSpeed + 'px';
+    }
+
+        requestAnimationFrame(mechanic)
 
 }
 mechanic();
-
-
-
-
-
