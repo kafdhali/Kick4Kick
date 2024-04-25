47a7e9abbfe5d1cfaf9bf5e295718d3263c152a8
kafdhali <khaledafdh075@gmail.com>
2024-04-25 22:37:54 +0200

    3.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("startButton").addEventListener("click", function () {
-    window.location.href = "index.html";
+    window.location.href = "game.html";
   });
 });
 const container = document.getElementById("container");
